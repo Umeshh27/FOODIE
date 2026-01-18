964e38539bbe476fb62f49834ce0e63a0982371c
Umeshh27 <23MH1A05E7@acoe.edu.in>
2026-01-18 18:33:54 +0530

    Added

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,32 @@
+import User from "../models/user.model.js"
+import bcrypt from "bcryptjs"
+
+const signUp = async (req,res) => {
+  try{
+    const{fullName,email,password,mobile,role}
+  =req.body  
+  const user=await User.findOne({email})
+  if(user){
+    return res.status(400).json({message:"User Already Exist."})
+  }
+  if(password.length<6){
+    return res.status(400).json({message:"Password Must Be Atleast 6 Characters."})
+  }
+  if(mobile.length<10){
+    return res.status(400).json({message:"Mobile Number Must Be Atleast 10 Digits."})
+  }
+
+  const hashedPassword= await bcrypt.hash(password,10)
+  user=await User.create({
+    fullName,
+    email,
+    role,
+    mobile,
+    password:hashedPassword
+  })
+  
+}
+  catch(error){
+
+  }
+}
