3832aab8eabb2747d13d07b963a3c32cd5140357
Umeshh27 <23MH1A05E7@acoe.edu.in>
2026-01-19 10:48:21 +0530

    Added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
 
 function App() {
    return( 
   <>
-  
+  <Routes>
+    <Route path='/signup' element={<SignUp/>} />
+    <Route path='/signin' element={<SignIn/>}></Route>
+  </Routes>
   </>
   )
 }
