aca1e334557d795b2ba33a1f8479d3e54e8d5453
Umeshh27 <23MH1A05E7@acoe.edu.in>
2026-01-21 09:36:12 +0530

    Added

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -8,6 +8,7 @@ function SignUp() {
   const bgColor='#fff9f6';
   const borderColor='#ddd';
   const [showPassword,setShowPassword]=useState(false);
+  const [role,SetRole]=useState("user")
   return (
     <>
     <div className='min-h-screen w-full flex items-center justify-center p-4' style={{background:bgColor}}>
@@ -41,6 +42,16 @@ function SignUp() {
             <input type={`${showPassword?"text":"password"}`} name="" id="" className='w-full border rounded-lg px-3 py-2 focus:outline-none'  placeholder='Enter Your Password' style={{border:`1px solid ${borderColor}`}}/>
 
             <button className='absolute right-3 top-[14px] text-gray-500 right-3 cursor-pointer' onClick={()=>setShowPassword(prev=>!prev)}>{!showPassword?<FaEye />:<FaEyeSlash />}</button>
+          </div>
+        </div>
+
+           {/* Role  */}
+        <div className='mb-4'>
+          <label htmlFor="Role" className='block text-gray-700 front-medium mb-1'>Role</label>
+          <div className='flex gap-2'>
+           {["user","owner","deliveryBoy"].map((r)=>(
+            <button className='flex-1 border rounded-lg px-3 py-2 text-center font-medium transition-colors'>{r}</button>
+           ))}
           </div>
         </div>
       </div>
